edfd8e04dac0fe42996a5c36c4e92be4f63cbc67
--local <wyiang@ucdavis.edu>
2026-03-10 14:47:06 -0700

    configure automatic refresh of debug page

--- debug_route.go
+++ debug_route.go
@@ -9,6 +9,7 @@ import (
 func addDebugRoute(mux *http.ServeMux, tracker *tracker.Tracker) {
 	mux.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Refresh", "5")
 		clients := tracker.GetServers()
 		debugPage(clients).Render(r.Context(), w)
 	})
