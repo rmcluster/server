280b670d47bf7dbe05b0c628422487934d27dff0
Wen Kai Yiang <wyiang@ucdavis.edu>
2026-04-29 11:20:13 -0700

    gcas: prototype interfaces

---
+++ server/gcas/gcas.go
@@ -0,0 +1,15 @@
+package gcas
+
+// GCAS is a content-addressible storage service that combines multiple CAS nodes into a single CAS.
+// It uses erasure coding to provide efficient redundancy.
+// The erasure coding used is Reed-Solomon coding.
+type GCAS interface {
+	CAS
+	AddNode(node NamedCAS)
+	RemoveNode(node NamedCAS)
+}
+
+type NamedCAS interface {
+	CAS
+	Name() string
+}
