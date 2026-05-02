21fab6c68139eaec82d99958f20ca30ac09f6a73
Wen Kai Yiang <wyiang@ucdavis.edu>
2026-05-01 20:29:32 -0700

    gcas: make OpenDB foolproof by removing version

--- server/gcas/db.go
+++ server/gcas/db.go
@@ -14,7 +14,13 @@ import (
 //go:embed migrations/*.sql
 var migrations embed.FS
 
-func OpenDB(dbPath string, version uint) (*sql.DB, error) {
+const dbVersion = 1
+
+func OpenDB(dbPath string) (*sql.DB, error) {
+	return OpenDBWithVersion(dbPath, dbVersion)
+}
+
+func OpenDBWithVersion(dbPath string, version uint) (*sql.DB, error) {
 	db, err := sql.Open("sqlite", dbPath+"?pragma=busy_timeout=10000")
 	if err != nil {
 		return nil, err
