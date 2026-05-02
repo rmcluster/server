971421b3910f179785d5b48af9372775510a07d6
Wen Kai Yiang <wyiang@ucdavis.edu>
2026-05-01 20:23:04 -0700

    gcas: add mock CAS data structure for testing purposes

---
+++ server/gcas/mock_cas.go
@@ -0,0 +1,88 @@
+package gcas
+
+import (
+	"context"
+	"crypto/sha256"
+	"sync"
+)
+
+func NewMockCAS() CAS {
+	return &mockCAS{
+		data: make(map[Hash][]byte),
+	}
+}
+
+type mockCAS struct {
+	mu   sync.RWMutex
+	data map[Hash][]byte
+}
+
+// Delete implements [CAS].
+func (m *mockCAS) Delete(ctx context.Context, hash Hash) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if _, ok := m.data[hash]; !ok {
+		return HashNotFoundError{}
+	}
+	delete(m.data, hash)
+	return nil
+}
+
+// FreeSpace implements [CAS].
+func (m *mockCAS) FreeSpace(ctx context.Context) (int64, error) {
+	return 1 << 30, nil
+}
+
+// Get implements [CAS].
+func (m *mockCAS) Get(ctx context.Context, hash Hash) ([]byte, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	// retreive data from CAS
+	data, ok := m.data[hash]
+
+	// if data is not found, return HashNotFoundError
+	if !ok {
+		return nil, HashNotFoundError{}
+	}
+
+	// if data is found, validate the hash
+	if !validateHash(hash, data) {
+		return nil, DataCorruptError{}
+	}
+
+	return data, nil
+}
+
+// List implements [CAS].
+func (m *mockCAS) List(ctx context.Context) (<-chan Hash, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	ch := make(chan Hash, len(m.data))
+	for k := range m.data {
+		ch <- k
+	}
+	close(ch)
+	return ch, nil
+}
+
+// Put implements [CAS].
+func (m *mockCAS) Put(ctx context.Context, hash Hash, data []byte) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if !validateHash(hash, data) {
+		return DataCorruptError{}
+	}
+	if _, ok := m.data[hash]; ok {
+		return HashExistsError{}
+	}
+	m.data[hash] = data
+	return nil
+}
+
+var _ CAS = (*mockCAS)(nil)
+
+// validateHash checks if the hash is the correct SHA256 hash of the data.
+func validateHash(h Hash, data []byte) bool {
+	return h == sha256.Sum256(data)
+}
---
+++ server/gcas/mock_cas_test.go
@@ -0,0 +1,129 @@
+package gcas
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"errors"
+	"testing"
+)
+
+func TestMockCASGetPut(t *testing.T) {
+	cas := NewMockCAS()
+	ctx := context.Background()
+
+	// test data
+	data := []byte("hello")
+
+	// hash data
+	hash := sha256.Sum256(data)
+
+	// put data in CAS
+	err := cas.Put(ctx, hash, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// get data from CAS
+	retrievedData, err := cas.Get(ctx, hash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// compare retrieved data with original data
+	if !bytes.Equal(data, retrievedData) {
+		t.Errorf("expected %s, got %s", data, retrievedData)
+	}
+
+	// test that CAS already has the data
+	err = cas.Put(ctx, hash, data)
+	if !errors.Is(err, HashExistsError{}) {
+		t.Errorf("expected HashExistsError, got %v", err)
+	}
+}
+
+// test deletion of CAS entry
+func TestMockCASDelete(t *testing.T) {
+	cas := NewMockCAS()
+	ctx := context.Background()
+
+	// test data
+	data := []byte("hello")
+
+	// hash test data
+	hash := sha256.Sum256(data)
+
+	// add data to CAS
+	err := cas.Put(ctx, hash, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// test that the CAS actually has the data
+	retrievedData, err := cas.Get(ctx, hash)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// compare retrieved data with original data
+	if !bytes.Equal(data, retrievedData) {
+		t.Errorf("expected %s, got %s", data, retrievedData)
+	}
+
+	// delete data from CAS
+	err = cas.Delete(ctx, hash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// test that data is deleted
+	_, err = cas.Get(ctx, hash)
+	if !errors.Is(err, HashNotFoundError{}) {
+		t.Errorf("expected HashNotFoundError, got %v", err)
+	}
+}
+
+func TestMockCASList(t *testing.T) {
+	cas := NewMockCAS()
+	ctx := context.Background()
+
+	// list should not return anything for empty CAS
+	{
+		list, err := cas.List(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		count := 0
+		for range list {
+			count++
+		}
+		if count != 0 {
+			t.Errorf("expected 0, got %d", count)
+		}
+	}
+
+	// create two test data entries
+	var testData []string = []string{"hello", "world"}
+
+	// add test data to the CAS
+	for _, data := range testData {
+		err := cas.Put(ctx, sha256.Sum256([]byte(data)), []byte(data))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// check that the CAS now has 2 entries
+	{
+		list, err := cas.List(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		count := 0
+		for range list {
+			count++
+		}
+		if count != 2 {
+			t.Errorf("expected 2, got %d", count)
+		}
+	}
+}
