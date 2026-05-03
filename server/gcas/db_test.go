4385792b2b45d1532cf52fcba0ac57474407cd4d
Wen Kai Yiang <wyiang@ucdavis.edu>
2026-05-02 21:49:00 -0700

    gcas: add test of reopening DB

--- server/gcas/db_test.go
+++ server/gcas/db_test.go
@@ -1,10 +1,31 @@
 package gcas
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+)
 
 func TestOpenDB(t *testing.T) {
 	// open a new in-memory db using OpenDB
 	db, err := OpenDB(":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.Close()
+}
+
+func TestDbReopen(t *testing.T) {
+	// open DB at a temporary path
+	tempdir := t.TempDir()
+	tempDbPath := filepath.Join(tempdir, "test.db")
+
+	db, err := OpenDB(tempDbPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.Close()
+
+	db, err = OpenDB(tempDbPath)
 	if err != nil {
 		t.Fatal(err)
 	}
